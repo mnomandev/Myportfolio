565b2821fb325f8fe4bd2b99588555626d91c150
nomandev <nomankhan02432@gmail.com>
2025-05-15 16:18:49 +0500

    testing commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="../../../public/Resume/resume.pdf"
+            href="/resume.pdf"
             target='_blank'
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
