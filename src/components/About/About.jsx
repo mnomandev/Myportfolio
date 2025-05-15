0ee861ffdaaa477df0ed43f182eedb168037e9b1
nomandev <nomankhan02432@gmail.com>
2025-05-15 15:48:09 +0500

    final commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           {/* Resume Button */}
           <a
             href={resume}
-            download="Resume"
+            download="resume"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
             style={{
